41717ea37cd7d82684d74e3618c8ea794e8b7129
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2024-05-21 09:21:02 -0400

    added stuff

--- helloWorld.py
+++ helloWorld.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
 
-# Created by: Kaitlyn I and Ava V
-# Created on: May 2024
-# This program is the "gamename" program on the PyBadge
 
-import ugame
+# Created by: Ava Venturino
+# Created on: Mar 2023
+# This program is the "space alien" game on pybadge
+
+
 import stage
+import ugame
+
+
+
 
 def game_scene():
-    # this function is the main game game_scene
+   #this function is the main game game_scene
 
-    print("n\n\n") # 3 blank lines
-    print("Hello, World!")
 
-    # repeat forever, game loop
-    while True:
-        pass # just a placeholder for now
+   print("\n\n\n") # 3 blank lines
+   print("Hello, World")
+
+
+   #repeat forever, game loop
+   while True:
+       pass # just a placeholder for now
+
+
+
 
 if __name__ == "__main__":
-    game_scene()
+   game_scene()
