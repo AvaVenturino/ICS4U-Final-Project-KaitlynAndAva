6e13d411e84be6d443f54df5e7a64c98e5a061b4
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2024-05-21 09:07:30 -0400

    added code to test

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,21 @@
-print('Hello, World!')
+#!/usr/bin/env python3
+
+# Created by: Kaitlyn I and Ava V
+# Created on: May 2024
+# This program is the "gamename" program on the PyBadge
+
+import ugame
+import stage
+
+def game_scene():
+    # this function is the main game game_scene
+
+    print("n\n\n") # 3 blank lines
+    print("Hello, World!")
+
+    # repeat forever, game loop
+    while True:
+        pass # just a placeholder for now
+
+if __name__ == "__main__":
+    game_scene()
